b5281fcb0eaec070cff8955d56cfe9826d580864
feranmiola <osunjuyigbeiyin@gmail.com>
2025-05-19 00:08:46 +0100

    Implement custom scrollbar functionality and add ContactUs component to Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,61 @@
+"use client";
+import ContactUs from "@/components/PageComponents/ContactUs";
 import Hero from "@/components/PageComponents/Hero";
 import Topbar from "@/components/Shared/Topbar";
 import Image from "next/image";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  useEffect(() => {
+    // Ensure this only runs in the browser
+    if (typeof window === "undefined") return;
+
+    let scrollTimer = 0;
+
+    // Function to update scrollbar properties
+    function updateScrollbar() {
+      const scrollPercentage =
+        window.scrollY /
+        (document.documentElement.scrollHeight - window.innerHeight);
+      const scrollbarHeight =
+        (window.innerHeight / document.documentElement.scrollHeight) *
+        window.innerHeight;
+      const scrollTop =
+        scrollPercentage * (window.innerHeight - scrollbarHeight);
+
+      document.body.style.setProperty("--scroll-top", `${scrollTop}px`);
+      document.body.style.setProperty(
+        "--scrollbar-height",
+        `${scrollbarHeight}px`
+      );
+
+      document.body.classList.add("is-scrolling");
+
+      clearTimeout(scrollTimer);
+      // @ts-ignore
+      scrollTimer = setTimeout(() => {
+        document.body.classList.remove("is-scrolling");
+      }, 1000);
+    }
+
+    // Add event listeners for scroll and resize
+    window.addEventListener("scroll", updateScrollbar);
+    window.addEventListener("resize", updateScrollbar);
+
+    // Initial call to set the correct scrollbar size
+    updateScrollbar();
+
+    // Cleanup event listeners on component unmount
+    return () => {
+      window.removeEventListener("scroll", updateScrollbar);
+      window.removeEventListener("resize", updateScrollbar);
+    };
+  }, []);
   return (
     <div className="flex flex-col w-screen relative  min-h-screen bg-[#F8F5F0]">
       <Topbar />
       <Hero />
+      <ContactUs />
     </div>
   );
 }
---
+++ src/components/PageComponents/ContactUs.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import LineContact from "../LineContact";
+
+const ContactUs = () => {
+  return (
+    <div className="w-full h-[666px] relative">
+      <div className="absolute left-0 bottom-0">
+        <LineContact />
+      </div>
+    </div>
+  );
+};
+
+export default ContactUs;
