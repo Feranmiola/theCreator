dc43aa8a79025233f06c7f5a2708ccaec5890aa7
feranmiola <osunjuyigbeiyin@gmail.com>
2025-05-19 00:11:52 +0100

    Refactor ContactUs component to include ContactUsTexts and adjust layout for improved styling

--- src/components/PageComponents/ContactUs.tsx
+++ src/components/PageComponents/ContactUs.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import LineContact from "../LineContact";
+import ContactUsTexts from "../ContactUsTexts";
 
 const ContactUs = () => {
   return (
-    <div className="w-full h-[666px] relative">
-      <div className="absolute left-0 bottom-0">
+    <div className="w-full h-[666px] relative flex items-center justify-center flex-col space-y-10">
+      <div className="absolute left-0 bottom-0 z-0">
         <LineContact />
       </div>
+      <ContactUsTexts />
+
+      <div className="w-[1192px] h-[340px] roudned-[24px] bg-white z-10"></div>
     </div>
   );
 };
