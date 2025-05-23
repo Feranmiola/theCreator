68111858ffa84a79773781ab78ef7a3d3e39b902
feranmiola <osunjuyigbeiyin@gmail.com>
2025-05-23 13:14:13 +0100

    Add JohnAvatar component to WeHaveTaught for enhanced user engagement, including a testimonial section featuring John Li.

--- src/components/PageComponents/WeHaveTaught.tsx
+++ src/components/PageComponents/WeHaveTaught.tsx
@@ -4,10 +4,11 @@ import LineHowTo1 from "../LineHowTo1";
 import LineHowTo2 from "../LineHowTo2";
 import ArrowLeft from "../ArrowLeft";
 import ArrowRight from "../ArrowRight";
+import JohnAvatar from "../JohnAvatar";
 
 const WeHaveTaught = () => {
   return (
-    <div className="w-full flex items-center justify-center flex-col">
+    <div className="w-full flex items-center justify-center flex-col pb-10">
       <div className="w-[1192px] flex flex-col space-y-[5rem]">
         <div className="flex w-full flex-col space-y-2">
           <div className="h-[67px] relative flex items-center  space-x-2">
@@ -58,6 +59,20 @@ const WeHaveTaught = () => {
 
             <div className="rounded-full border border-brightPink cursor-pointer hover:bg-brightPink transition-all ease-in-out p-2 flex items-center justify-center">
               <ArrowRight />
+            </div>
+          </div>
+        </div>
+
+        <div className="flex flex-row w-full">
+          <div className="w-[508px] h-[352px] bg-[#FFADF2] rounded-[16px] flex items-center justify-center">
+            <div className="flex flex-col justify-evenly w-[396px] h-full">
+              <JohnAvatar />
+              <p className="font-medium text-[32px] text-deepGreen">John Li</p>
+              <p className="leading-2 text-deepGreen text-xl">
+                The Creator innovative ideas, strategic approach, and
+                outstanding results have left a lasting impression on me as a
+                blogger, making them a standout in the creative industry.
+              </p>
             </div>
           </div>
         </div>
