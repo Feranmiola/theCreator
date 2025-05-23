a9c9566de18cba712fb6ef0e7d6c20b41ea130b4
feranmiola <osunjuyigbeiyin@gmail.com>
2025-05-23 13:45:55 +0100

    Update Footer component styling for improved layout and text emphasis

--- src/components/PageComponents/Footer.tsx
+++ src/components/PageComponents/Footer.tsx
@@ -2,10 +2,10 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="w-full flex items-center justify-center">
+    <div className="w-full flex items-center justify-center pt-10">
       <div className="w-[1192px] flex items-center justify-center flex-col space-y-[5rem]">
         <div className="flex flex-col space-y-2 items-center justify-center">
-          <p className="text-deepGreen text-[56px] fnot-bold">
+          <p className="text-deepGreen text-[56px] font-bold">
             We are waiting for you to contact us
           </p>
           <p className="text-deepGreen text-xl">
