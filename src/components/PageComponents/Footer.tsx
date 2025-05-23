4cca593b1111faaab276de98072a262e102bc4ef
feranmiola <osunjuyigbeiyin@gmail.com>
2025-05-23 13:46:43 +0100

    Add email subscription input to Footer component for enhanced user engagement

--- src/components/PageComponents/Footer.tsx
+++ src/components/PageComponents/Footer.tsx
@@ -12,6 +12,16 @@ const Footer = () => {
             You can write to us at any time and get an instant response.
           </p>
         </div>
+
+        <div className="flex flex-row w-[567px] h-[64px] bg-white rounded-[100px] items-center space-x-2 p-2">
+          <input
+            placeholder="Enter Your Email"
+            className="bg-transparent outline-none focus:outline-none placeholder:text-[#D4D4D4] text-deepGreen w-full flex flex-1 pl-2 text-xl"
+          />
+          <div className="cursor-pointer bg-brightPink text-deepGreen rounded-[100px] font-semibold text-[18px] w-[102px] h-full flex items-center justify-center hover:bg-transparent border border-transparent hover:border-brightPink transition ease-in-out">
+            Join
+          </div>
+        </div>
       </div>
     </div>
   );
